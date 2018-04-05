e9b6fe4e314b836dc80ac9540cadb06de07d5c58
Stenin.Denis <blackkwade@gmail.com>
2018-04-05 16:02:31 +0400

    - Added dynamical camera; - Added score text;

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
+#include <sstream>
 #include "map.h"
+#include "view.h"
 using namespace sf;
 
 CircleShape shape(50);
@@ -16,13 +18,18 @@ struct pos{
 };
 
 class PlayerSnake{
+private:
+    float x = 0;
+    float y = 0;
+
 public:
-   float x, y, w, h, dx, dy, speed;
+   float w, h, dx, dy, speed;
     int dir = 0;
     String File;
     Image image;
     Texture texture;
     Sprite sprite;
+    int playerScore;
 
     PlayerSnake(String F, int X, int Y, float W, float H){
         File = F;
@@ -33,7 +40,8 @@ public:
         sprite.setTexture(texture);
         x = X;
         y = Y;
-        sprite.setTextureRect(IntRect(0, 0, w, h));
+        sprite.setTextureRect(IntRect(190, 0, w, h));
+        playerScore = 0;
     }
 
     void update(float time){
@@ -51,14 +59,55 @@ public:
         y+= dy*time;
 
         sprite.setPosition(x, y);
+        mapRules();
+    }
+
+    float getPlayerCoordinateX(){
+        return x;
+    }
+    float getPlayerCoordinateY(){
+        return y;
+    }
+
+    void mapRules(){
+        for(int i = y / 32; i < (y + h) / 32; i++){
+            for(int j = x / 32; j < (x + w) / 32; j++){
+                if(TileMap[i][j] == '0'){
+                    if(dy > 0){
+                        y = i * 32 - h;
+                    }
+                    if(dy < 0){
+                        y = i * 32 + 32;
+                    }
+                    if(dx > 0){
+                        x = j * 32 - w;
+                    }
+                    if(dx < 0){
+                        x = j * 32 + 32;
+                    }
+                }
+                if(TileMap[i][j] == 's'){
+                    TileMap[i][j] = ' ';
+                    playerScore++;
+                }
+            }
+        }
     }
 
 };
 
 int main(){
     RenderWindow window(VideoMode(640, 480), "Snake");
+    view.reset(FloatRect(0, 0, 640, 480));
+
     Clock clock;
-    PlayerSnake player("snake.png", 250, 250, 60, 60);
+    PlayerSnake player("snake.png", 200, 200, 65, 65);
+
+    Font font;
+    font.loadFromFile("../Images/CyrilicOld.TTF");
+    Text text(" ", font, 20);
+    text.setColor(Color::Green);
+    text.setStyle(Text::Bold);
 
     Image mapimage;
     mapimage.loadFromFile("../Images/map.png");
@@ -82,21 +131,28 @@ int main(){
         if ((Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))){
             player.dir = 1;
             player.speed = 0.1;
+            player.sprite.setTextureRect(IntRect(190, 64, 65, 64));
         }
         if ((Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))){
             player.dir = 0;
             player.speed = 0.1;
+            player.sprite.setTextureRect(IntRect(260, 0, 70, 64));
         }
         if ((Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))){
             player.dir = 2;
             player.speed = 0.1;
+            player.sprite.setTextureRect(IntRect(260, 65, 65, 65));
         }
         if ((Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))){
             player.dir = 3;
             player.speed = 0.1;
+            player.sprite.setTextureRect(IntRect(190, 0, 65, 65));
         }
+        getPlayerCoordinateView(player.getPlayerCoordinateX(), player.getPlayerCoordinateY());
+
         player.update(time);
 
+        window.setView(view);
         window.clear(Color(102, 153, 255));
 
         for(int i = 0; i < HEIGHT_MAP; i++){
@@ -110,6 +166,12 @@ int main(){
                 window.draw(mapsprite);
             }
         }
+        std::ostringstream playerScoreString;
+        playerScoreString << player.playerScore;
+        text.setString("Player score:" + playerScoreString.str());
+        text.setPosition(view.getCenter().x - 165, view.getCenter().y - 200);
+        window.draw(text);
+
         window.draw(player.sprite);
         window.display();
 
